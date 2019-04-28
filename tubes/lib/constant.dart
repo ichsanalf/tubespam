c9521f6fd714ccdf06f4915d2e76a4e88673e8e4
putriyolandastp <putri.11416161@student.itera.ac.id>
2019-04-28 22:51:42 +0700

    constant

---
+++ tubes/lib/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:color/color.dart';
+
+class DiaryPallete{
+  static RgbColor black = const RgbColor(4, 4, 4);
+  static RgbColor undo = const RgbColor(194, 190, 190);
+}
