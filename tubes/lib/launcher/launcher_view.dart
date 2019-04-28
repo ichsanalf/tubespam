df756336589dad836ea5be20574cdeb8a6fcd483
putriyolandastp <putri.11416161@student.itera.ac.id>
2019-04-28 12:09:08 +0700

    Launcher

---
+++ tubes/lib/launcher/launcher_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:tubes/landingpage/landingpage_view.dart';
+
+class LauncherPage extends StatefulWidget{
+  @override
+  _LauncherPageState createState() => new _LauncherPageState();
+}
+
+class _LauncherPageState extends State<LauncherPage> {
+  @override
+
+  void initState(){
+    ///metodh ini mirip seperti onCreate()
+    super.initState();
+    startLaunching();
+  }
+
+  startLaunching() async {
+    var duration = const Duration(seconds: 1); //set durasi untuk splashscreen
+    return new Timer(duration, ()
+    {
+      Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (_) {
+        /* push, pushReplaceName, pushReplacement = cara berpindah halaman.
+        menggunakan pushReplacement karena jika user menekan back maka activity sebelumnya
+        tidak akan ditampilkan lagi atau finish.
+        */
+        return new LandingPage();
+        /* akan memanggil class setTimer pada initState,
+        pada timer terdapat fungsi yang akan dijalankan ketika timer selesai
+        */
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: new Center(
+        child: new Image.asset(
+          "asset/img_logo.png", //memanggil logo image pada assets
+          height: 100.0,
+          width: 200.0,
+        ),
+      ),
+    );
+  }
+
+}
+
+
+
